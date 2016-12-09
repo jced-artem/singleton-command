d7982cc4749c5b7d7cd37a2587c5bfa1793a3c7b
Artem <artem.jcd@gmail.com>
2016-12-09 11:48:43 +0200

    Create SingletonCommand.php

---
+++ SingletonCommand.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Jced;
+
+use Jced\LockService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class SingletonCommand
+ */
+abstract class SingletonCommand extends Command
+{
+    /** @var string */
+    protected $name = null;
+
+    /** @var string */
+    private $lockName = null;
+
+    /** @var LockService */
+    private $lockService;
+
+    /**
+     * SingletonCommand constructor.
+     * @param LockService $lockService
+     * @throws \Exception
+     */
+    public function __construct(LockService $lockService)
+    {
+        $this->lockService = $lockService;
+        if (empty($this->name)) {
+            throw new \Exception('Empty command name');
+        }
+        parent::__construct();
+    }
+
+    /**
+     * @param $name
+     */
+    protected function setLockName($name)
+    {
+        $this->lockName = $name;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    protected function beforeLock(InputInterface $input, OutputInterface $output)
+    {
+        return ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    abstract public function lockExecute(InputInterface $input, OutputInterface $output);
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     * @throws \Exception
+     */
+    final protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->beforeLock($input, $output);
+        if (empty($this->lockName)) {
+            $this->lockName = $this->name;
+        }
+        if (!$this->lockService->lock($this->lockName)) {
+            throw new \Exception('Already launched command [ ' . $this->lockName .  ' ]');
+        }
+        register_shutdown_function([$this->lockService, 'release'], $this->lockName);
+        return $this->lockExecute($input, $output);
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function release()
+    {
+        return $this->lockService->release($this->name);
+    }
+
+    protected function configure()
+    {
+        $this->setName($this->name);
+    }
+}
